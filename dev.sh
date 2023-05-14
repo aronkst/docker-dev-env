76a9f4e5a9f74ede822571620ab35866b2e4a35a
Aron <aron.0504@hotmail.com>
2023-05-14 17:26:57 -0300

    dev.sh file added

---
+++ dev.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+git clone https://github.com/asdf-vm/asdf.git ~/.asdf --branch v0.11.3
+echo '' >> ~/.bashrc
+echo '. "~/.asdf/asdf.sh"' >> ~/.bashrc
+
+source ~/.asdf/asdf.sh
+
+asdf plugin add ruby
+asdf install ruby latest
+asdf global ruby latest
+
+gem install rails
+
+asdf plugin add erlang
+asdf install erlang latest
+asdf global erlang latest
+
+asdf plugin add elixir
+asdf install elixir latest
+asdf global elixir latest
+mix local.hex --force
+
+mix archive.install --force hex phx_new
+
+asdf plugin add nodejs
+asdf install nodejs latest
+asdf global nodejs latest
+
+asdf plugin add golang
+asdf install golang latest
+asdf global golang latest
+
+asdf plugin add python
+asdf install python latest
+asdf global python latest
+
+curl --proto '=https' --tlsv1.2 -sSf -y https://sh.rustup.rs | sh
